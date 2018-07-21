b7b2279f529cda8facf951943d187ea3716465bc
ncortes12 <ncortes1415@gmail.com>
2018-07-20 17:59:20 -0700

    function customer app

--- bamazoncustomer.js
+++ bamazoncustomer.js
@@ -3,8 +3,10 @@ var keys = require('./keys.js');
 var mysql = require('mysql');
 var inquirer = require('inquirer');
 var mySqlKey = keys.mySQL
+var newQuantity = 0;
+var cost = 0;
 
-// console.log(mySqlKey.host, mySqlKey.port, mySqlKey.user, mySqlKey.password, mySqlKey.database)
+console.log(mySqlKey.host, mySqlKey.port, mySqlKey.user, mySqlKey.password, mySqlKey.database)
 
 var connection = mysql.createConnection({
     host: mySqlKey.host,
@@ -14,9 +16,11 @@ var connection = mysql.createConnection({
     database: mySqlKey.database
 
 });
- connection.connect(function (err, res) {
-        if (err) throw err;
-        console.log("Connected to " + connection.config.database);})
+connection.connect(function (err, res) {
+    if (err) throw err;
+    console.log("Connected to " + connection.config.database);
+    startStore();
+})
 
 function startStore() {
     inquirer.prompt([
@@ -28,41 +32,95 @@ function startStore() {
         }
     ]).then(function () {
         if ("confirm") {
-            buyItems()
+            chooseItems()
 
         }
     })
 }
 
-function buyItems() {
-   
-inquirer.prompt([
+function chooseItems() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        console.log("Items available for purchase ")
+        for (var i = 0; i < res.length; i++) {
+            console.log("Item: " + res[i].product_name + "|" + "Price: $" + res[i].price + "|" + "Quantity Available: " + res[i].stock_quantity);
+        }
+
+
+        inquirer.prompt([
             {
                 name: "choice",
-                type: "rawlist",
+                type: "list",
                 choices: function () {
                     var choicesArray = [];
-                    connection.query("SELECT * FROM products", function (err, res) {
-                        if (err) throw err;
-                        
-                        for (var i = 0; i < res.length; i++) {
-                            choicesArray.push(res[i].product_name);
-                        }
-                        console.log(choicesArray);
-                        return choicesArray;
-                        
-                    })
+                    for (var i = 0; i < res.length; i++) {
+                        choicesArray.push(res[i].product_name);
 
-
+                    }
+                    // console.log(choicesArray);
+                    return choicesArray;
                 },
                 message: "What item would you like to buy?"
+            },
+            {
+                type: "input",
+                message: "How many would you like?",
+                name: "quantity"
             }
-        ])}
+
+        ]).then(function (answer) {
+            // console.log(answer);
+            connection.query("SELECT * FROM products WHERE ?", { product_name: answer.choice }, function (err, res) {
+                if (err) throw err;
+                // console.log(res);
+                // console.log(answer);
+                if (answer.quantity <= res[0].stock_quantity) {
+                    var numberChoice = Number(answer.quantity)
+                    // console.log(numberChoice);
+                    cost = numberChoice * res[0].price;
+                    newQuantity = res[0].stock_quantity - numberChoice;
+
+                    //   var check =  typeof(newQuantity);
+                    //   var check2 = typeof(numberChoice);
+                    //   console.log(check, check2);
+                    //   console.log(newQuantity)
+                    // console.log(newQuantity);
+                    updateInventory(answer);
+                }
+                else {
+                    console.log("Sorry we don't have enough!")
+                    startStore();
+                }
+            })
+
+        })
+    }
+    )
+}
+
+function updateInventory(answer) {
+    // console.log(answer);
 
 
-startStore();
+    connection.query("UPDATE products SET ? WHERE ?",
+        [
+            {
+                stock_quantity: newQuantity
+            },
+            {
+                product_name: answer.choice
+            }
+        ],
+        function (err, res) {
+            if (err) throw err;
+            console.log("You just bought a bunch of neat stuff! Your total is $"+ cost + ". Have a nice day");
+            startStore();
+        })
 
-// buyItems();
+}
+
+
+
 
 
 
@@ -76,7 +134,7 @@ startStore();
 //         for (var i = 0; i < res.length; i++) {
 //             console.log("Item: " + res[i].product_name + "\n"+ "Price: " + res[i].price + "\n");
 //         }
-        
+
 //     })}
 // test();
 
